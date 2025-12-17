e95df426b226cedfb933c6b760c14a481f05914d
Jaya Patel <jayamyname@gmail.com>
2025-12-17 15:30:39 +0530

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return(
+
+    <div className="formbox">
+
+    <h1>Login Page</h1>
+
+    <section>
+    <label>Name:</label>
+    <input type='text' placeholder='Enter  your Name'/> 
+</section>
+
+       
+<section>
+    <label>Email:</label>
+    <input type='email' placeholder='Enter  your email'/>
+</section>
+
+<section>
+    <label>Password:</label>
+    <input type='password' placeholder='Enter  your Password'/>
+</section>
+
+<br></br>
+
+<button type='submit'>Submit</button>
+    </div>
+
+)
+}
+
+export default App
